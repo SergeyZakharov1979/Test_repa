a9553418ada20f572b346ecc476c7cf7bee02dc7
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-08 20:06:46 +0700

    max and min

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 n = int(input())
+maximum = 0
+minimum = 10**30
 
 while n > 0:
     last_num = n % 10
-    print(last_num, end='')
+
+    if last_num > maximum:
+        maximum = last_num
+    if last_num < minimum:
+        minimum = last_num
+
     n //= 10
+
+print(f'Максимальная цифра равна {maximum}', f'Минимальная цифра равна {minimum}', sep='\n')
