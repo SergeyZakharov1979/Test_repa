648b1c1ada1d5e010538792db033de1689533845
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-10 17:35:20 +0700

    Звёздный треугольник вариант 2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 n = int(input())
 
-for i in range(1, n // 2 + 2):
-    print('*' * i)
+# for i in range(1, n // 2 + 2):
+#     print('*' * i)
 
-for j in range(n // 2, 0, - 1):
-    print('*' * j)
+# for j in range(n // 2, 0, - 1):
+#     print('*' * j)
+
+
+cnt = n // 2
+
+for _ in range(n):
+    for _ in range(n // 2 - abs(cnt)):
+        print('*', end='')
+    print()
+    cnt -= 1
