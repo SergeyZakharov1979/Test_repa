ddadc363378511ba5354d982ca70d68844ecc293
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-12 18:25:59 +0700

    Делители 1

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-n = int(input())
+a, b = int(input()), int(input())
+max_div, summ, cur_summ = 1, 1, 0
 
-for i in range(n):
-    cnt = 0
-    for j in range(i + 1):
-        cnt +=1
-        print(cnt, end='')
+for i in range(a, b + 1):
+    cur_summ = 0
+    for j in range(1, b + 1):
+        if i % j == 0:
+            cur_summ +=j
 
-    for k in range(i, 0, - 1):
-        cnt -= 1
-        print(cnt, end='')
-    print()
+    if cur_summ >= summ:
+        max_div = i
+        summ = cur_summ
+
+print(max_div, summ)
