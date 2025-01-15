a0ea5f31cbf521519065138b99e47d7a2ee878d5
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-15 15:12:55 +0700

    Сколько символов в нижнем регистре

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-s, cnt_vowels, cnt_consonants = input(), 0, 0
-volwels, consonants = 'ауоыиэяюе', 'бвгджзйклмнпрстфхцчшщ'
+s, cnt = input(), 0
 
-for c in s:
-    if c.lower() in volwels:
-        cnt_vowels += 1
-    if c.lower() in consonants:
-        cnt_consonants += 1
-    
-print(f'Количество гласных букв равно {cnt_vowels}')
-print(f'Количество согласных букв равно- {cnt_consonants}')
+while len(s) > 0:
+    if s[-1].isalpha():
+        if s[-1] == s[-1].lower():
+            cnt += 1
+    s = s[:-1]
 
+print(cnt)
