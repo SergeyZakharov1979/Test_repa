5b97e2e00ce13015e6d9d2677b0fe3f720d4b11f
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-15 19:03:59 +0700

    Самый частотный символ

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 s = input()
-cnt = s.count(' ') + 1
+frq_repit_smbl = ''
+cnt_frq_repit_smbl = 0
 
-print(cnt)
+while len(s) > 0:
+    cur_symb = s[0]
+    cnt_cur_symb = s.count(s[0])
+
+    if cnt_cur_symb >= cnt_frq_repit_smbl:
+        cnt_frq_repit_smbl = cnt_cur_symb
+        frq_repit_smbl = cur_symb
+
+    s = s.replace(cur_symb, '')
+
+print(frq_repit_smbl)
