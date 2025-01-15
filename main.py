269e9e8c725a726386f4dc88a27c6eae9f0e5a17
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-15 15:23:16 +0700

    Сколько слов с строке

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-s, cnt = input(), 0
-
-while len(s) > 0:
-    if s[-1].isalpha():
-        if s[-1] == s[-1].lower():
-            cnt += 1
-    s = s[:-1]
+s = input()
+cnt = s.count(' ') + 1
 
 print(cnt)
