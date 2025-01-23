b92c085cf5980498c27b81f46dd354cf2fdc8df8
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-23 13:22:46 +0700

    Алфавит

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
+lst = []
+for i in range(26):
+    j = i + 1
+    lst.append(chr(ord('a') + i) * j)
 
-n = int(input())
-
-book = input().split(maxsplit=2)
-autor = book[0]
-title = book[2]
-flag = "YES"
-print(autor, title)
-for _ in range(n - 1):
-    book = input().split(maxsplit=2)
-    print(book)
-    if book[0] > autor:
-        continue
-    elif book[0] == autor:
-        if book[2] > title:
-            continue
-        else:
-            flag = 'NO'
-            break
-    else:
-        flag = 'NO'
-        break
-print(flag)
+print(lst)
