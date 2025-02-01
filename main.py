f925e7c76a22c3ffa1393ccfdba827cddc014ba2
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-02-01 19:04:15 +0700

    sorted bubles

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(*[el for el in input() if el.isdigit()], sep='')
+a = [5, 1, 4, 2, 8]
+cnt = 0
+
+while cnt != len(a) - 1:
+    for i in range(1, len(a)):
+        if a[i-1] > a[i]:
+            a[i], a[i-1] = a[i-1], a[i] 
+    cnt += 1
+
+print(a)
