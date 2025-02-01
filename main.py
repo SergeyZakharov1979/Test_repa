d08590c56aad29ea48c5f51b150ae714379063e8
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-02-01 18:14:48 +0700

    List comprehensions 2

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally',
-            'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
-
-new_keywords = [l[1:] for l in keywords]
-
-print(new_keywords)
+print(*[el for el in input() if el.isdigit()], sep='')
