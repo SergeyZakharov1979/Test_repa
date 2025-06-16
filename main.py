f93f4e006dadb61d49ab8bc3e6b78d36b1ce11e2
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-06-16 21:17:05 +0700

    chenged file main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-a, b = int(input()), int(input())
-
-while a <= b:
-    if a % 777 == 0:
-        break
-    if a % 2 == 0 or a % 3 == 0:
-        a += 1
-        continue
-    print(a)
-    a += 1
+s = 0
+while (el := int(input())) != 0:
+    s += el
+print(s)
